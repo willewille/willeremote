fa0b04eb501327c491a442c55785ad9558798f7b
willewille <56308919+willewille@users.noreply.github.com>
2022-07-17 20:58:32 +0200

    Add files via upload

---
+++ remote.py
@@ -0,0 +1,27 @@
+from gpiozero import Motor
+from time import sleep
+
+m1 = Motor(27, 22)
+m2 = Motor(21, 20)
+
+while(1):
+    
+    x=input()
+    
+    
+    if x=='f':
+        m1.forward()
+        m2.forward()
+        
+    if x=='s':
+       m1.backward()
+       m2.forward()
+        
+    if x=='c':
+        m1.stop()
+        m2.stop()
+        
+    if x=='stop':
+        m1.stop()
+        m2.stop()
+        quit()
